927ffe3cf2a57371fc5749b73e08dc7dfa4bbbad
khs2 <hyunsuki20@gmail.com>
2024-11-22 08:49:45 +0900

    -clean app.js 라우터,서버설정 부분 코드정리

--- server/app.js
+++ server/app.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+const db = require('./services/databaseService');
 const { initializeDatabase } = require('./services/databaseService');
 
 app.use(cors({
-    origin : '*',
-    methods : ['GET','DELETE'],
-    allowedHeaders : ['Content-Type','Authorization'],
+  origin: '*', // 모든 도메인에서 접근 허용
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'], // 필요한 헤더만 허용
 }));
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
+  res.sendStatus(200);
+});
 // 라우터 등록
 const rankingsRoutes = require('./routes/rankingsRoutes');
 const usersRoutes = require('./routes/usersRoutes');
+app.use((req, res, next) => {
+  res.setHeader('Content-Type', 'application/json');
+  next();
+});
 
 app.use('/rankings', rankingsRoutes);
 app.use('/users', usersRoutes);
 
 app.get('/', async (req, res) => {
-    await initializeDatabase();
-    res.send("data reset");
+  await initializeDatabase();
+  res.send("data reset");
 });
 
 module.exports = app;
