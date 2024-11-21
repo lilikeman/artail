5bc55017ca08f854383d8518a1dbcd4314a5a871
khs2 <hyunsuki20@gmail.com>
2024-11-22 08:55:09 +0900

    -clean rankings조회 주석추가

--- server/controllers/rankingsController.js
+++ server/controllers/rankingsController.js
@@ -1,10 +1,19 @@
 const { getAllQuery } = require('../utils/dbUtils');
-const {db} = require('../services/databaseService');
+const { db } = require('../services/databaseService');
 
+
+
+// author : 김현수
+// edit date : 2024-11-22
+// last editor : 김현수
+// 사용자 랭킹 조회
+// Desc: page를 입력하여 레벨X경험치 순으로 사용자정보를 100개출력함
+// TODO: 1)레벨x경험치 역순정렬 2) limit 추가 3)
 exports.getRankings = async (req, res) => {
     const { page = 1, limit = 100 } = req.query;
     const offset = (page - 1) * limit;
     try {
+        // 1. 랭킹정보 조회
         const rankings = await getAllQuery(db, `
             SELECT 
                 userId, 
@@ -22,15 +31,15 @@ exports.getRankings = async (req, res) => {
             ORDER BY level DESC, experience DESC
             LIMIT ? OFFSET ?
         `, [limit, offset]);
-
+        // 2.사용자 숫자 조회
         const totalCount = await getAllQuery(db, `
             SELECT COUNT(*) as count FROM Users WHERE isDeleted = false
         `, []);
-        
+
+        // 데이터베이스가 없어졌을 경우, 유저정보가 없어졌을 경우
         if (rankings.length === 0) {
             return res.status(404).json({ status: 404, message: '랭킹이 정보가 없습니다.' });
         }
-
         res.json({
             status: 200,
             data: {
