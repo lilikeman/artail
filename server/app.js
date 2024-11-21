fc609526c1d93439382f3f919adcccb70895a9bb
khs2 <hyunsuki20@gmail.com>
2024-11-22 08:50:01 +0900

    empty commit

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const db = require('./services/databaseService');
 const { initializeDatabase } = require('./services/databaseService');
 
+//flutter 웹앱은 데이터를 crossorigin으로 통신하기떄문에 cors설정 추가
 app.use(cors({
   origin: '*', // 모든 도메인에서 접근 허용
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -15,9 +16,12 @@ app.options('*', (req, res) => {
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
   res.sendStatus(200);
 });
+
 // 라우터 등록
 const rankingsRoutes = require('./routes/rankingsRoutes');
 const usersRoutes = require('./routes/usersRoutes');
+
+//prefilight 관련
 app.use((req, res, next) => {
   res.setHeader('Content-Type', 'application/json');
   next();
@@ -26,6 +30,7 @@ app.use((req, res, next) => {
 app.use('/rankings', rankingsRoutes);
 app.use('/users', usersRoutes);
 
+//서버 데이터 베이스 초기화
 app.get('/', async (req, res) => {
   await initializeDatabase();
   res.send("data reset");
