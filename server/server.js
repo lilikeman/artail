258b172e6c5e4d3421ee99fee79c932d42af9d57
khs2 <hyunsuki20@gmail.com>
2024-11-22 08:45:57 +0900

    -clean 서버정보코드정리

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
-const port = process.env.PORT || 3000;
+const port =  3000;
 
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
     console.log(`Server is running`);
 });
