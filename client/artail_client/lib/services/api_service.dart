0c582612e776108bc2bd1c7d5e632c0638db9d95
HSK <hs.kim@oceansbio.com>
2024-11-22 09:45:39 +0900

    -update http호출 서버직접 -> 미들웨어로호출방식으로 수정

--- client/artail_client/lib/services/api_service.dart
+++ client/artail_client/lib/services/api_service.dart
@@ -1,21 +1,28 @@
 import 'package:dio/dio.dart';
+
 import '../models/user.dart';
 
 class ApiService {
-  final Dio _dio = Dio();
+  final Dio _dio = Dio(BaseOptions(
+    headers: {
+      'Content-Type': 'application/json',
+      "Accept": "*/*",
+    },
+  ));
   int maxPage = 1;
+
   Future<List<User>> fetchUsers(int page) async {
     try {
       final response = await _dio.get(
-        'http://15.165.170.112:3000/rankings',  
+        'https://wvdk20k7eg.execute-api.ap-northeast-2.amazonaws.com/Prod/rankings',
         queryParameters: {
           'page': page,
         },
       );
-
       if (response.statusCode == 200) {
-        List<dynamic> data = response.data['data']['rankings'];
-        maxPage = response.data['data']['pagination']['totalPages'];
+        print(response.data);
+        List<dynamic> data = response.data['data']['data']['rankings'];
+        maxPage = response.data['data']['data']['pagination']['totalPages'];
         return data.map((userData) => User.fromJson(userData)).toList();
       } else {
         throw Exception('Failed to load ranking');
@@ -24,17 +31,20 @@ class ApiService {
       throw Exception('Failed to fetch users: $e');
     }
   }
+
   Future<List<User>> searchUsers(String keyword) async {
     try {
       final response = await _dio.get(
-        'http://15.165.170.112:3000/users/search',  
+        'https://wvdk20k7eg.execute-api.ap-northeast-2.amazonaws.com/Prod/users/search',
         queryParameters: {
           'keyword': keyword,
         },
       );
 
       if (response.statusCode == 200) {
-        List<dynamic> data = response.data['data']['users'];
+                print(response.data);
+
+        List<dynamic> data = response.data['data']['data']['users'];
         return data.map((userData) => User.fromJson(userData)).toList();
       } else {
         throw Exception('검색결과 x');
@@ -43,28 +53,29 @@ class ApiService {
       throw Exception('$e');
     }
   }
+
   Future<String> deleteUsers(String userId) async {
     try {
       final response = await _dio.delete(
-        'http://15.165.170.112:3000/users/$userId', 
+        'https://wvdk20k7eg.execute-api.ap-northeast-2.amazonaws.com/Prod/users',
         queryParameters: {
           'userId': userId,
         },
       );
+        print(response.data);
 
       if (response.statusCode == 200) {
-        String message = response.data['message'];
+        String message = response.data['data']['message'];
         return message;
       } else {
-        throw Exception('Failed to delete id');
+        throw Exception('아이디 삭제실패');
       }
     } catch (e) {
-      throw Exception('Failed to delete id: $e');
+      throw Exception('아이디 삭제실패: $e');
     }
   }
 
-
-  int getMaxPage(){
+  int getMaxPage() {
     return maxPage;
   }
 }
